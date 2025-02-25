cb3fd8bdeb79e74c873ebbc9b941d26eba579b5f
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-02-25 16:21:13 +0800

    Complete cal_rhs

--- p2.py
+++ p2.py
@@ -1,15 +1,30 @@
-def find_Bi(pi, msg_list):
-    max = 0
+import math
+
+def cal_rhs(i, msg_list, tau):
+    higher_priority = list()
+    lower_equal_priority = list()
+
+    # Split the list into higher and lower/equal priority
     for msg in msg_list:
-        if msg.p < pi and msg.c > max:
-            max = msg.c
-    return max
+        if msg.p < msg_list[i].p:
+            higher_priority.append(msg)
+        else:
+            lower_equal_priority.append(msg)
 
-def sum_wtime(i):
-    return i
+    # Colculate Bi
+    Bi = 0
+    for msg in lower_equal_priority:
+        if msg.c > Bi:
+            Bi = msg.c
 
-def cal_Qi(msg_list, tau):
-    return find_Bi(msg_list) + sum_wtime(msg_list, tau)
+    # Calculate the sum of waiting time
+    sum_wtime = 0
+    for msg in higher_priority:
+        sum_wtime += math.ceil((Bi + tau) / msg.t) * msg.c
+
+    rhs = Bi + sum_wtime
+
+    return rhs
 
 class message:
     def __init__(self, p, c, t):
