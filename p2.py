c0d6735f8c0704c702d06e8fff52c9a58b957752
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2025-02-25 22:51:00 +0800

    Complete the structure of cal_ri, need to deal with Qi initialize (Bi)

--- p2.py
+++ p2.py
@@ -11,7 +11,7 @@ def cal_rhs(i, msg_list, tau):
         else:
             lower_equal_priority.append(msg)
 
-    # Colculate Bi
+    # Calculate Bi
     Bi = 0
     for msg in lower_equal_priority:
         if msg.c > Bi:
@@ -26,6 +26,18 @@ def cal_rhs(i, msg_list, tau):
 
     return rhs
 
+def cal_ri(i, msg_list, tau, qi):
+
+    rhs = cal_rhs(i, msg_list, tau)
+
+    if rhs + msg_list[i].c > msg_list[i].t:
+        return -1
+    elif rhs == qi:
+        return qi + msg_list[i].c
+    else:
+        cal_ri(i, msg_list, tau, rhs)
+
+
 class message:
     def __init__(self, p, c, t):
         self.p = p
@@ -38,7 +50,7 @@ if __name__ == "__main__":
         with open("input.dat", "r") as file:
             data = file.readlines()
             msg_len = int(data[0].strip())
-            tau = int(data[1].strip())
+            tau = float(data[1].strip())
 
             msg_list = list()
 
@@ -46,6 +58,10 @@ if __name__ == "__main__":
                 values = line.strip().split()
                 msg_list.append(message(int(values[0]), float(values[1]), int(values[2])))
 
+            for i in range(msg_len):
+                ri = cal_ri(i, msg_list, tau, )
+                print(ri)
+                
     except FileNotFoundError:
         print("input.dat file not found.")
     except ValueError:
